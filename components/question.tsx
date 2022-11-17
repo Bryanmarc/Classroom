4861a7303b9eea459bbc2a4d8b0267414267d79e
Cammie <you@example.com>
2022-11-17 17:29:15 -0500

    modified:   components/header.tsx 	modified:   components/question.tsx 	modified:   components/task.tsx 	modified:   pages/main.tsx 	modified:   styles/globals.css 	modified:   yarn.lock

--- components/question.tsx
+++ components/question.tsx
@@ -8,14 +8,17 @@ function Question(props: any) {
             <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
             <div className="container">
-                <a style={{ color: "black" }}> {props.studentAsked } </a>
+                <a  className="sName"> <strong> {props.studentAsked } </strong> </a>
                 <span className="timeRight">{props.timeAsked}</span>
                 <br />
                 {props.children}
-                <br />
-                <a className=" reply mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
-                   reply
+                <form>
+                    <input className="replyBar" type="text" placeholder="send a reply?"/> 
+                    <a className=" reply mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                   <button>reply</button>
                 </a>
+                </form>
+                <br />
             </div>
 
         </>
