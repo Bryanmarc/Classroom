3b0cada5c67d48cafcfee1623e982bb38437a3fa
William Thompson <wtth225@uky.edu>
2022-11-17 19:13:07 -0500

    - created start to taskboard page - added goal component

--- components/goal.tsx
+++ components/goal.tsx
@@ -1,7 +1,18 @@
+import Task from "../components/task"
 function Goal(props: any) {
 
     return (
         <>
+            <div className="goalContainer demo-card-square mdl-card mdl-shadow--2dp">
+                <h1 className="taskText"> {props.goalName} </h1>
+                <details>  
+                <summary> 
+                    Tasks required for this goal
+                </summary>
+                    <Task assignedTo={props.taskAssignTo} taskname={props.taskAssignName}> </Task>
+                </details>
+            </div>
+
 
 
         </>
