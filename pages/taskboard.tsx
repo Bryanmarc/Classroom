a271b06229dbf1dff3d6fb9972ac9feddfc8c905
William Thompson <wtth225@uky.edu>
2022-11-17 19:13:07 -0500

    - created start to taskboard page - added goal component

--- pages/taskboard.tsx
+++ pages/taskboard.tsx
@@ -3,10 +3,20 @@ import Goal from "../components/goal"
 export default function main() {
     return (
         <>
-            <Header pageType="Dashboard">
+            <Header pageType="Taskboard">
             </Header>
 
-            <Goal> </Goal>
+            <div style={{ display: "flex", flex: '1' }}>
+                <div style={{ backgroundColor: "gray", width: '350px', marginLeft: '10px', marginBottom: '5px', gridArea: "auto", alignSelf: "flex-start" }}>
+                    <h4 style={{ textAlign: "center" }}> Your assigned tasks: </h4>
+
+                    <Goal goalName="Add compatibility for IOS devices" taskAssignTo="William" taskAssignName="Create Iphone"> </Goal>
+                    <Goal goalName="Add compatibilty for Floridian devices"> </Goal>
+
+                </div>
+            </div>
+
+
 
         </>
     )
