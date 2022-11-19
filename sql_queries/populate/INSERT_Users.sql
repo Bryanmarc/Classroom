a49fe5615afcf7c184d556a392e7656ebcd0f69d
Tanner Jones <tejo235@uky.edu>
2022-11-18 21:28:21 -0500

    Added Goals and Users to SQL tables

---
+++ sql_queries/populate/INSERT_Users.sql
@@ -0,0 +1,42 @@
+INSERT INTO Users (
+    first_name,
+    last_name,
+    username,
+    team_id,
+    role
+  )
+VALUES (
+    'Austin',
+    'Topping',
+    'Ajto238',
+    '1',
+    'STUDENT'
+  );
+INSERT INTO Users (
+    first_name,
+    last_name,
+    username,
+    team_id,
+    role
+  )
+  VALUES (
+    'Tanner',
+    'Jones',
+    'TEJO235',
+    '1',
+    'STUDENT'
+  );
+INSERT INTO Users (
+    first_name,
+    last_name,
+    username,
+    team_id,
+    role
+  )
+VALUES (
+    'Cammie',
+    'Barnes',
+    'CCBA236',
+    '1',
+    'STUDENT'
+  );
